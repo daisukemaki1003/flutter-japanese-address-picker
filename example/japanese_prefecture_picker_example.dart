806ff352a077a704d62d61686b432450588ed85d
牧大佑 <daisukemaki1003@gmail.com>
2023-03-04 20:35:41 +0900

    パッケージを作成

---
+++ example/japanese_prefecture_picker_example.dart
@@ -0,0 +1,6 @@
+import 'package:japanese_prefecture_picker/japanese_prefecture_picker.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/japanese_prefecture_picker.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library japanese_prefecture_picker;
+
+export 'src/japanese_prefecture_picker_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
