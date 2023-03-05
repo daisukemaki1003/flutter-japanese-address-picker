90bc6edebf765f54af2ddf5b2b87ba11bf2920fe
牧大佑 <daisukemaki1003@gmail.com>
2023-03-05 15:10:43 +0900

    [add] sample project

---
+++ example/android/app/src/main/kotlin/com/example/japanese_prefecture_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.japanese_prefecture_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- example/japanese_prefecture_picker_example.dart
+++
@@ -1,3 +0,0 @@
-import 'package:japanese_prefecture_picker/japanese_prefecture_picker.dart';
-
-void main() {}
