1b8168bd40d4bf35d9108ca0e858ef4ce1e341e2
牧大佑 <daisukemaki1003@gmail.com>
2023-03-05 19:16:03 +0900

    [add] picker

--- lib/src/japanese_prefecture_picker.dart
+++ lib/src/japanese_prefecture_picker.dart
@@ -1,13 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:japanese_prefecture_picker/src/utils/assets_loader.dart';
 
 class JapanesePrefecturePicker extends StatefulWidget {
   const JapanesePrefecturePicker({
     Key? key,
     this.header,
+    this.itemExtent = 35.0,
+    this.onChange,
   }) : super(key: key);
 
   /// ピッカーのヘッダー
   final Widget? header;
+
+  /// リストピッカーのアイテム範囲
+  /// デフォルト値は[35.0]
+  final double itemExtent;
+
+  /// ピッカー項目間を移動するときに呼び出されるNullable関数
+  /// 選択された都道府県市データを返します
+  final Function(dynamic)? onChange;
 
   void show(BuildContext context) {
     showModalBottomSheet(
@@ -31,6 +43,14 @@ class JapanesePrefecturePicker extends StatefulWidget {
 }
 
 class _JapanesePrefecturePickerState extends State<JapanesePrefecturePicker> {
+  final List<String> items = [];
+
+  @override
+  void initState() {
+    AssetsLoader.prefectures();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -40,7 +60,15 @@ class _JapanesePrefecturePickerState extends State<JapanesePrefecturePicker> {
         children: [
           widget.header ?? Container(),
           Expanded(
-            child: Container(),
+            child: CupertinoTheme(
+              data: CupertinoThemeData(textTheme: CupertinoTextThemeData()),
+              child: CupertinoPicker(
+                itemExtent: widget.itemExtent,
+                scrollController: FixedExtentScrollController(),
+                onSelectedItemChanged: (i) => widget.onChange?.call(items[i]),
+                children: items.map((e) => Text(e)).toList(),
+              ),
+            ),
           ),
         ],
       ),
